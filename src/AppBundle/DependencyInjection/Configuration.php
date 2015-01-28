f27a2bbe667468b23b04e3cdf691b14eef417443
jonasdw <jonas.dewint@gmail.com>
2015-01-28 16:10:57 +0100

    Fix broken parameters

--- src/AppBundle/DependencyInjection/Configuration.php
+++ src/AppBundle/DependencyInjection/Configuration.php
@@ -20,6 +20,11 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('app_bundle_app');
 
+        $rootNode
+            ->children()
+                ->scalarNode('camera_manager_storage_path')->defaultValue('')->end()
+            ->end();
+
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
